aaeeb72ae49056a49b494b0e0610626fa1798f80
Abdelrahman Salama <rahman.salama78@gmail.com>
2026-06-09 17:04:08 +0200

    fix: restore correct test assertion

--- app/app.test.ts
+++ app/app.test.ts
@@ -27,7 +27,7 @@ describe('GET /ping', () => {
       url: '/ping',
     });
 
-    expect(response.statusCode).toBe(201);
+    expect(response.statusCode).toBe(200);
     expect(JSON.parse(response.body)).toEqual({ count: 42 });
   });
 
